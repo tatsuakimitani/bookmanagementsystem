7db64a3953145eb64a0389dc3684780e544b32ed
tatsuakimitani <t.mitani1988@gmail.com>
2017-01-16 19:29:57 +0900

    ソース追加

---
+++ Book-Management-System/src/main/java/jp/co/acroquest/BookManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package jp.co.acroquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementSystemApplication.class, args);
+	}
+}
---
+++ Book-Management-System/src/main/java/jp/co/acroquest/controller/BookRegistrationController.java
@@ -0,0 +1,14 @@
+package jp.co.acroquest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookRegistrationController
+{
+    @RequestMapping("/")
+    public String home()
+    {
+        return "/list";
+    }
+}
