de05b03e0bbba9a4676839aafcc827b8442fac77
tatsuakimitani <t.mitani1988@gmail.com>
2017-02-18 19:33:38 +0900

    コントローラ追加

--- Book-Management-System/src/main/java/jp/co/acroquest/controller/BookRegistrationController.java
+++ Book-Management-System/src/main/java/jp/co/acroquest/controller/BookRegistrationController.java
@@ -6,9 +6,84 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BookRegistrationController
 {
-    @RequestMapping("/")
+    /**
+     * 書籍情報一覧画面を表示
+     * @return 書籍情報一覧画面
+     */
+    @RequestMapping("/list")
     public String home()
     {
         return "/list";
     }
+
+    /**
+     * 書籍情報詳細画面を表示
+     * @return 書籍情報詳細画面
+     */
+    @RequestMapping("/detail")
+    public String detail()
+    {
+        return "/detail";
+    }
+
+    /**
+     * 貸出状況変更画面を表示
+     * @return 貸出状況変更画面
+     */
+    @RequestMapping("/borrow")
+    public String borrow()
+    {
+        return "/borrow";
+    }
+
+    /**
+     * 書籍情報登録画面を表示
+     * @return 書籍情報登録画面
+     */
+    @RequestMapping("/create")
+    public String create()
+    {
+        return "/create";
+    }
+
+    /**
+     * 書籍情報登録確認画面を表示
+     * @return 書籍情報登録確認画面
+     */
+    @RequestMapping("/createConfirm")
+    public String createConfirm()
+    {
+        return "/createConfirm";
+    }
+
+    /**
+     * 書籍情報更新画面を表示
+     * @return 書籍情報更新画面
+     */
+    @RequestMapping("/update")
+    public String update()
+    {
+        return "/update";
+    }
+
+    /**
+     * 書籍情報更新確認画面を表示
+     * @return 書籍情報更新確認画面
+     */
+    @RequestMapping("/updateConfirm")
+    public String updateConfirm()
+    {
+        return "/updateConfirm";
+    }
+
+    /**
+     * 書籍情報削除確認画面を表示
+     * @return 書籍情報削除確認画面
+     */
+    @RequestMapping("/deleteConfirm")
+    public String deleteConfirm()
+    {
+        return "/deleteConfirm";
+    }
+
 }
---
+++ Book-Management-System/src/main/resources/static/js/app.js
@@ -0,0 +1,21 @@
+angular.module('bookManagementApp', []).controller('ListController', ['$scope', function ($scope) {
+  $scope.dataList = [{
+    no: 1,
+    title: 'JUnit実践入門',
+    author: '渡辺 修司',
+    price: 12000,
+    publisher: 'WEB+DB PRESS plus',
+    category: 'コンピュータ・IT',
+    isBorrowed: false,
+    published: new Date(2016, 1, 18)
+  }, {
+    no: 2,
+    title: '仕事の思想',
+    author: '田坂 広志',
+    price: 15000,
+    publisher: 'PHP文庫',
+    category: '文学・評論',
+    isBorrowed: false,
+    published: new Date(2016, 4, 18)
+  }];
+}]);
