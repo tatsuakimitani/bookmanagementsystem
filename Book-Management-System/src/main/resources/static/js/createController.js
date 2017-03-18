82505a78c687c72ddcfdef376ff3c1ac8b95af57
tatsuakimitani <t.mitani1988@gmail.com>
2017-03-18 16:15:14 +0900

    登録画面を更新

--- Book-Management-System/src/main/resources/static/js/createController.js
+++ Book-Management-System/src/main/resources/static/js/createController.js
@@ -1,6 +1,31 @@
-angular.module('bookManagementApp', []).controller('RegisterBookController', ['$scope',function ($scope) {
+/**
+ * 登録画面用のコントローラ
+ */
+angular.module('bookManagementApp', []).controller('RegisterBookController', ['$scope', function ($scope) {
   var self = this;
-  $scope.register = function (book) {
-    console.log(book);  // 確認用
+  $scope.submitForm = function () {
+    if ($scope.registerForm.$invalid) {
+      alert('our form is amazing');
+    } else {
+      self.submitRegisterForm($scope);
+    }
+  };
+
+  // $scope.register = function (scope) {
+  //   // 必須項目が入力されていない場合はエラーを返す。
+  //   if (!book || !book.name
+  //     || !book.author
+  //     || !book.publisher
+  //     || !book.category) {
+  //     alert("エラーです");
+  //   } else {
+  //     self.submitRegisterForm(scope.book.name);
+  //   }
+  // };
+
+  // 登録情報を取得し、登録確認画面に遷移させる。
+  self.submitRegisterForm = function (scope) {
+    console.log("register");
+    console.log(scope.book.name);
   };
 }]);
